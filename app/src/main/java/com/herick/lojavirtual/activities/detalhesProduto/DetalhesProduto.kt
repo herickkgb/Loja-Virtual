6c2e132b561f54887aef42b4c6bbd9c951067250
Herick Pereira <42222113+herickkgb@users.noreply.github.com>
2023-11-27 19:52:49 -0300

    implementando detalhes produtos

--- app/src/main/java/com/herick/lojavirtual/activities/detalhesProduto/DetalhesProduto.kt
+++ app/src/main/java/com/herick/lojavirtual/activities/detalhesProduto/DetalhesProduto.kt
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.herick.lojavirtual.R
+import com.herick.lojavirtual.activities.FormLogin.FormLogin
+import com.herick.lojavirtual.activities.pagamento.FormPagamento
 import com.herick.lojavirtual.databinding.ActivityDetalhesProdutoBinding
 
 class DetalhesProduto : AppCompatActivity() {
     private lateinit var binding: ActivityDetalhesProdutoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         binding = ActivityDetalhesProdutoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -24,5 +28,16 @@ class DetalhesProduto : AppCompatActivity() {
             .load(urlImagem)
             .into(binding.imgDtFotoProduto)
 
+
+
+
+        clickButtonFinalizarPedido(binding)
+    }
+
+    private fun clickButtonFinalizarPedido(binding: ActivityDetalhesProdutoBinding) {
+        binding.btfinalizarPedido.setOnClickListener {
+            val intent = Intent(this, FormPagamento::class.java)
+            startActivity(intent)
+        }
     }
 }
